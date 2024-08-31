574cf9e05eef15fb7bfeb8412aaa9f5d8a742a27
saja123 <saja_fawaz@yahoo.com>
2024-08-31 22:38:07 +0300

    make  SubjectDto

---
+++ SCMS-back-end/Models/Dto/SubjectDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SCMS_back_end.Models.Dto
+{
+    public class SubjectDto
+    {
+        public int SubjectId { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+
+    }
+}
