b1ca6f76370966fd139cf513b1e83262216042b7
MohammadFarwan <alfrwan58@gmail.com>
2024-09-05 16:10:28 +0300

    edit level

--- SCMS-back-end/Repositories/Services/StudentService.cs
+++ SCMS-back-end/Repositories/Services/StudentService.cs
@@ -121,7 +121,7 @@ namespace SCMS_back_end.Repositories.Services
             }
 
             // Check if student level is appropriate
-            if (student.Level == course.Level)
+            if (student.Level > course.Level || student.Level < course.Level)
             {
                 throw new InvalidOperationException($"Student level ({student.Level}) is not equal the course level ({course.Level}).");
             }
