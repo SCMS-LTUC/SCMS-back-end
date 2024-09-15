e4a606aecdc1148e9d0de510e937ea9b9e31b632
Dima Salem <demamohamad1998@gmail.com>
2024-09-15 10:11:28 +0300

    upd

--- SCMS-back-end/Data/StudyCenterDbContext.cs
+++ SCMS-back-end/Data/StudyCenterDbContext.cs
@@ -164,6 +164,16 @@ namespace SCMS_back_end.Data
            new IdentityRole { Id = "3", Name = "Student", NormalizedName = "STUDENT" }
             );
 
+            //modelBuilder.Entity<WeekDay>().HasData(
+            //    new WeekDay { WeekDayId = 1,   Name="Saturday" },
+            //    new WeekDay { WeekDayId = 2, Name = "Sunday" },
+            //    new WeekDay { WeekDayId = 3, Name = "Monday" },
+            //    new WeekDay { WeekDayId = 4, Name = "Tuesday" },
+            //    new WeekDay { WeekDayId = 5, Name = "Wednesday" },
+            //    new WeekDay { WeekDayId = 6, Name = "Thursday" },
+            //    new WeekDay { WeekDayId = 7, Name = "Friday" }
+
+            //    );
 
         }
 
--- SCMS-back-end/Migrations/StudyCenterDbContextModelSnapshot.cs
+++ SCMS-back-end/Migrations/StudyCenterDbContextModelSnapshot.cs
@@ -433,7 +433,7 @@ namespace SCMS_back_end.Migrations
                     b.HasIndex("AssignmentId", "StudentId")
                         .IsUnique();
 
-                    b.ToTable("StudentAssignment", (string)null);
+                    b.ToTable("StudentAssignments", (string)null);
                 });
 
             modelBuilder.Entity("SCMS_back_end.Models.StudentCourse", b =>
@@ -769,13 +769,13 @@ namespace SCMS_back_end.Migrations
             modelBuilder.Entity("SCMS_back_end.Models.StudentAssignment", b =>
                 {
                     b.HasOne("SCMS_back_end.Models.Assignment", "Assignment")
-                        .WithMany("StudentAssignment")
+                        .WithMany("StudentAssignments")
                         .HasForeignKey("AssignmentId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
                     b.HasOne("SCMS_back_end.Models.Student", "Student")
-                        .WithMany("StudentAssignment")
+                        .WithMany("StudentAssignments")
                         .HasForeignKey("StudentId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
@@ -836,7 +836,7 @@ namespace SCMS_back_end.Migrations
 
             modelBuilder.Entity("SCMS_back_end.Models.Assignment", b =>
                 {
-                    b.Navigation("StudentAssignment");
+                    b.Navigation("StudentAssignments");
                 });
 
             modelBuilder.Entity("SCMS_back_end.Models.Course", b =>
@@ -872,7 +872,7 @@ namespace SCMS_back_end.Migrations
                 {
                     b.Navigation("LectureAttendances");
 
-                    b.Navigation("StudentAssignment");
+                    b.Navigation("StudentAssignments");
 
                     b.Navigation("StudentCourses");
                 });
