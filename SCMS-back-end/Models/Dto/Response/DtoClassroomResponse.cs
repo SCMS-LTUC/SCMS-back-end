bcb69ecef443ede8cb62889b3b85c7b6831a36de
Mohammed Alzoubi <Mohammed.alzoubi.dev@gmail.com>
2024-09-20 19:07:00 +0300

    add Classroom

---
+++ SCMS-back-end/Models/Dto/Response/DtoClassroomResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SCMS_back_end.Models.Dto.Response
+{
+    public class DtoClassroomResponse
+    {
+        public int ClassroomId { get; set; }
+        public string RoomNumber { get; set; }
+        public int Capacity { get; set; }
+    }
+}
